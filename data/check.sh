d508f4ac88c98d0d745bf35964b147d15274e2ae
Dmitry Marakasov <amdmi3@amdmi3.ru>
2011-12-20 22:03:56 +0400

    Add extra check

--- data/check.sh
+++ data/check.sh
@@ -11,4 +11,7 @@ for f in $files; do
 	sed -e 's| *#.*$||' < $f | grep -Eiv '[XIV]+.*съезда' | grep '[acekmopuxyABCEHKMOPTXY]' | grep '.' && echo "\--------- Non-cyrillic letters found"
 	sed -e 's| *#.*$||' < $f | uniq -d | grep '.' && echo "\--------- Duplicates found"
 	grep -E '.#.*(XXX|FIXME)' $f && echo "\--------- XXX/FIXME items found"
+	for x in улица переулок проезд проспект аллея площадь; do
+		grep -E "^[^#]*$x[^#]*$x" $f && echo "\--------- likely, newline missing"
+	done
 done
