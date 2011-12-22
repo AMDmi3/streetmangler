4e5f2cf01fe498c4f972848b9238d675d49a8725
Dmitry Marakasov <amdmi3@amdmi3.ru>
2011-12-22 21:52:53 +0400

    Reorder blocks

--- data/check.sh
+++ data/check.sh
@@ -8,9 +8,9 @@ fi
 
 for f in $files; do
 	echo "===> $f"
+	grep -E '.#.*(XXX|FIXME)' $f && echo "\--------- XXX/FIXME items found"
 	sed -e 's| *#.*$||' < $f | grep -Eiv '[XIV]+.*съезда' | grep '[acekmopuxyABCEHKMOPTXY]' | grep '.' && echo "\--------- Non-cyrillic letters found"
 	sed -e 's| *#.*$||' < $f | uniq -d | grep '.' && echo "\--------- Duplicates found"
-	grep -E '.#.*(XXX|FIXME)' $f && echo "\--------- XXX/FIXME items found"
 	for x in улица переулок проезд проспект аллея площадь; do
 		grep -E "^[^#]*$x[^#]*$x" $f | grep -v 'Автопроезд' | grep '.' && echo "\--------- likely, newline missing"
 	done
